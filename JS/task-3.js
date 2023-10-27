901bc013f7d15b79b68e67b6a98754940ace1420
Dok6857 <141351808+Dok6857@users.noreply.github.com>
2023-10-28 00:51:45 +0300

    third task complete

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,19 +1,19 @@
-"use strict"
+'use strict';
 
-function checkForSpam(message) {
-    const messageLow = message.toLowerCase();
-    const hasSpam = messageLow.includes("spam");
-    const saleMessage = messageLow.includes("sale");
+function filterArray(numbers, value) {
+  const numbersArray = [];
 
-    return hasSpam || saleMessage ? true : false;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      numbersArray.push(numbers[i]);
+    }
+  }
 
+  return numbersArray;
 }
 
-
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
